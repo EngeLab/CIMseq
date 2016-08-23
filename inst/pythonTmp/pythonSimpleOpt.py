5354a8688d7250f62cc9b5ee28c0ccfc57aa389f
jasonserviss <jason.serviss@ki.se>
2016-08-23 19:29:25 +0200

    work on simple optimisation example

--- inst/pythonTmp/pythonSimpleOpt.py
+++ inst/pythonTmp/pythonSimpleOpt.py
@@ -1,64 +1,42 @@
 import numpy as np
 from pyswarm import pso
+import math
 
-fractions = []
-for l in range(0, 3):
-    fractions.append(1/np.float64(3))
-fractions = np.asarray(fractions)
-
-cellTypes = np.asarray([50, 50, 0])
-slice = [100]
-
+"""
+   A really simple example with 3 possible cell types and only one gene. 
+   The "multuplet" (or slice) value for the gene is the mean of the 2 contributing cell types.
+"""
+#define fractions, cellTypes, and slice
+fractions = [1/np.float64(3)] * 3
+cellTypes = np.asarray([25, 75, 0])
+slice = [50]
 
 #define function to minimize
-def makeSyntheticSlice(cellTypes, fractions):
-    return sum(cellTypes * fractions)
-
 def distToSlice(fractions, *args):
-    cellTypes, slice = args
-    a = makeSyntheticSlice(cellTypes, fractions)
-    if math.isnan(a):
-        print "NaN in make synthetic slice!"
-        print a
-    
-    cost = abs(a - slice)
-    return cost
+    return abs(slice - sum(fractions * cellTypes))
 
-
-#define constraints
-"""f_ieqcons : function
-    Returns a 1-D array in which each element must be greater or equal to 0.0 in a successfully optimized problem. If f_ieqcons is specified, ieqcons is ignored (Default: None)"""
-
-#fractions sum to 1
+#define constraint; fractions sum to 1
 def con1(fractions, *args):
     if sum(fractions) == 1:
-        return 0
+        return [0]
     else:
-        return -1
+        return [-1]
 
-#constrains each fraction to be 0 or > the fractions if all cell types were divided equally among the multuplet
-def con2(fractions, *args):
-    limit = 1/np.float64(len(fractions))
-    oc1 = list(set([i for i in fractions if i < limit and i != 0.0]))
-    if sum([Counter(fractions)[k] for k in oc1]) == 0:
-        return 0
-    else:
-        return -1
-
-def constraints(fractions, *args):
-    cons1 = con1(fractions, *args)
-    cons2 = con2(fractions, *args)
-    return [cons1, cons2]
+#test distToSlice with fractions expected after optimization; expect cost to be 0
+print distToSlice(np.asarray([0.5, 0.5, 0]), (cellTypes, slice))
 
 #define optimization function
-def optimize(fractions, cellTypes, slice, col):
+def optimize(fractions, cellTypes, slice):
     lb = np.asarray([0] * len(fractions))
     ub = np.asarray([1] * len(fractions))
-    args = (cellTypes, slice, col)
-    xopt, fopt = pso(distToSlice, lb, ub, args=args, f_ieqcons=constraints, maxiter=500)
-    dictionary = dict(zip(list(cellTypes.columns.values), xopt.tolist()))
-    return { 'xopt':dictionary, 'fopt':fopt }
+    args = (cellTypes, slice)
+    xopt, fopt = pso(distToSlice, lb, ub, args=args, f_ieqcons=con1, maxiter=10000, swarmsize=1000)
+    return { 'xopt':xopt, 'fopt':fopt }
 
+#run optimization; expected to result in fractions = [0.5, 0.5, 0.0] and cost = 0
+result = optimize(fractions, cellTypes, slice)
+print result
 
-result0 = optimize(fractions, cellTypes, slice, 0)
-
+"""
+    This is typically resulting as one of the fractions = 1 and the remaining = 0 even though the cost for this solution is higher than the cost for the desired solution.
+"""
