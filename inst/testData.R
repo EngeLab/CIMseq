2433e72e1eaa3065d2ed5179c55e53d162dea9c1
jasonserviss <jason.serviss@ki.se>
2016-09-21 16:34:22 +0200

    altered assembleTestData function to run spUnsupervised internally; finished and ran noiseData function

--- inst/testData.R
+++ inst/testData.R
@@ -1,12 +1,16 @@
 
 
 assembleTestData <- function() {
+    
+    #load data
     load('data/expData.rda')
     load('data/unsupervised.rda')
     
     counts <- getData(expData, "counts")
     sampleType <- getData(expData, "sampleType")
     sng <- counts[ ,sampleType == "Singlet"]
+    
+    unsupervised <- spUnsupervised(cObj, max=2000, max_iter=20000)
     
     classification <- getData(unsupervised, "mclust")$classification
     means <- getData(unsupervised, "groupMeans")
@@ -38,6 +42,18 @@ assembleTestData <- function() {
         names <- c(names, name)
     }
     
+    #quadruplets
+    comb <- combn(unique(classification), 4)
+    
+    for( i in 1:ncol(comb)) {
+        currentMult <- rowMeans(data.frame(means[ ,comb[1,i]], means[ ,comb[2,i]], means[ ,comb[3,i]], means[ ,comb[4,i]]))
+        name <- paste(comb[1,i], comb[2,i], comb[3,i], comb[4,i], sep=".")
+        
+        dataset <- cbind(dataset, currentMult)
+        names <- c(names, name)
+    }
+    
+    #adjust colnames and save
     colnames(dataset) <- paste("m.", names, sep="")
     colnames(sng) <- paste("s.", classification, sep="")
     testData <- as.matrix(cbind(sng, dataset))
@@ -132,38 +148,69 @@ syntheticTestData <- function(save=FALSE) {
     return(list(singlets, multuplets))
 }
 
-noiseData <- function() {
+noiseData <- function(n=3) {
     data(syntheticData)
+    
+    #subset multuplets
     multuplets <- syntheticData[ ,grep("m.", colnames(syntheticData))]
     
     tmp <- multuplets[ ,nchar(colnames(multuplets)) == 6]
-    two <- tmp[ ,sample(1:ncol(tmp), 3, replace=FALSE)]
+    two <- tmp[ ,sample(1:ncol(tmp), n, replace=FALSE)]
     
     tmp <- multuplets[ ,nchar(colnames(multuplets)) == 8]
-    three <- tmp[ ,sample(1:ncol(tmp), 3, replace=FALSE)]
+    three <- tmp[ ,sample(1:ncol(tmp), n, replace=FALSE)]
     
     tmp <- multuplets[ ,nchar(colnames(multuplets)) == 10]
-    four <- tmp[ ,sample(1:ncol(tmp), 3, replace=FALSE)]
+    four <- tmp[ ,sample(1:ncol(tmp), n, replace=FALSE)]
     
-    noise <- array(NA, dim=c(nrow(multuplets), 3, 101, 3))
-    dimnames(noise)[[3]] <- as.character(seq(0.00,1,0.01))
+    #declare array to hold noise data
+    noise <- array(NA, dim=c(nrow(multuplets), n, 11, 3))
+    dimnames(noise)[[3]] <- as.character(seq(0.00,1,0.1))
     dimnames(noise)[[4]] <- c("two", "three", "four")
     
     set.seed(11)
-    percent <- seq(0.01,1,0.01)
+    percent <- seq(0.1,1,0.1)
     
+    #add data with no noise
+    noise[,,1, 'two'] <- two
+    noise[,,1, 'three'] <- three
+    noise[,,1, 'four'] <- four
+
     #two
-    for( i in 1:100 ) {
+    for( i in 1:length(percent) ) {
         tmp <- two
         nRows <- nrow(syntheticData) * percent[i]
         picked <- sample(1:nrow(multuplets), nRows, replace = FALSE)
         sub <- two[picked, ]
-        reordered <- t(sapply(1:nrow(sub), function(x) sub[x,][sample(1:3, 3, replace=FALSE)]))
+        reordered <- t(sapply(1:nrow(sub), function(x) sub[x,][sample(1:n, n, replace=FALSE)]))
         tmp[picked, ] <- reordered
         noise[,,i+1, 'two'] <- tmp
-        
-        ##add names
     }
+    
+    #three
+    for( i in 1:length(percent) ) {
+        tmp <- three
+        nRows <- nrow(syntheticData) * percent[i]
+        picked <- sample(1:nrow(multuplets), nRows, replace = FALSE)
+        sub <- three[picked, ]
+        reordered <- t(sapply(1:nrow(sub), function(x) sub[x,][sample(1:n, n, replace=FALSE)]))
+        tmp[picked, ] <- reordered
+        noise[,,i+1, 'three'] <- tmp
+    }
+    
+    #four
+    for( i in 1:length(percent) ) {
+        tmp <- four
+        nRows <- nrow(syntheticData) * percent[i]
+        picked <- sample(1:nrow(multuplets), nRows, replace = FALSE)
+        sub <- four[picked, ]
+        reordered <- t(sapply(1:nrow(sub), function(x) sub[x,][sample(1:n, n, replace=FALSE)]))
+        tmp[picked, ] <- reordered
+        noise[,,i+1, 'four'] <- tmp
+    }
+    
+    save(noise, file='data/noiseData.rda', compress="bzip2")
+    return(noise)
 }
 
 
