b6c5baaeb6b120495bd77e38af5c5e9ee2252884
jasonserviss <jason.serviss@ki.se>
2016-10-19 14:36:02 +0200

    still trying to setup appropriate vignette and testing data

--- inst/testData.R
+++ inst/testData.R
@@ -10,18 +10,20 @@ save(expData, file='data/expData.rda', compress='bzip2')
 
 #unit test and vignettes data
 
+.ntopVar <- function(data, n) {
+    rv = apply(data, 1, var)
+    select = order(rv, decreasing=TRUE)[1:n]
+    return(select)
+}
+
+
 testData <- syntheticData[,1:1000]
 
-A1 <- testData[ ,grepl("s.A1", colnames(testData))][1:2000,1:100]
-B1 <- testData[ ,grepl("s.B1", colnames(testData))][1:2000,1:100]
-C1 <- testData[ ,grepl("s.C1", colnames(testData))][1:2000,1:100]
-D1 <- testData[ ,grepl("s.D1", colnames(testData))][1:2000,1:100]
-E1 <- testData[ ,grepl("s.E1", colnames(testData))][1:2000,1:100]
-F1 <- testData[ ,grepl("s.F1", colnames(testData))][1:2000,1:15]
-G1 <- testData[ ,grepl("s.G1", colnames(testData))][1:2000,1:15]
-H1 <- testData[ ,grepl("s.H1", colnames(testData))][1:2000,1:15]
-I1 <- testData[ ,grepl("s.I1", colnames(testData))][1:2000,1:15]
-J1 <- testData[ ,grepl("s.J1", colnames(testData))][1:2000,1:15]
+A1 <- testData[ ,grepl("s.A1", colnames(testData))][1:2000,1:25]
+B1 <- testData[ ,grepl("s.B1", colnames(testData))][1:2000,1:25]
+C1 <- testData[ ,grepl("s.C1", colnames(testData))][1:2000,1:25]
+D1 <- testData[ ,grepl("s.D1", colnames(testData))][1:2000,1:25]
+E1 <- testData[ ,grepl("s.E1", colnames(testData))][1:2000,1:25]
 
 mult <- c("m.A1B1", "m.B1C1", "m.C1D1", "m.D1E1")
 testData <- cbind(A1, B1, C1, D1, E1, syntheticData[1:nrow(A1) ,mult])
@@ -34,13 +36,13 @@ ercc <- matrix(c(rep(rep(9, 15), rep(4.5, 3), 3), 2), nrow=2)
 cObj <- spCounts(testData, ercc, "m.")
 
 ##spUnsupervised
-uObj <- spUnsupervised(cObj, max=2000, max_iter=2000, Gmax=5)
+uObj <- spUnsupervised(cObj, max=1000, max_iter=7000, Gmax=5)
 spPlot(uObj, type="clusters")
 table(getData(uObj, "classification"))
 
 #spSwarm
-sObj <- spSwarm(cObj, uObj, cores=4, swarmsize=250, maxiter=10)
-
+sObj <- spSwarm(uObj, cores=4, swarmsize=250, maxiter=10)
+getData(sObj, "spSwarm")
 
 ##sanity check
 cObj <- spCounts(syntheticData, matrix(), "m.")
