4587366eda8b95aa8267a1a53fd10bcb30e2f56d
jasonserviss <jason.serviss@ki.se>
2017-04-27 16:17:27 +0200

    same as last commit

--- inst/testData.R
+++ inst/testData.R
@@ -1,7 +1,6 @@
 library(sp.scRNAseqTesting)
 
 #expData
-
 load('inst/counts.rda')
 load('inst/counts.ercc.rda')
 
@@ -11,15 +10,7 @@ load('inst/counts.ercc.rda')
 expCounts <- counts
 expErcc <- counts.ercc
 
-suffix <- c()
-for(i in 1:ncol(counts)) {
-    if(grepl('1000102901', colnames(counts)[i])) {
-        suffix <- c(suffix, 'm')
-    } else {
-        suffix <- c(suffix, 's')
-    }
-}
-
+suffix <- ifelse(grepl('1000102901', colnames(counts)), "s", "m")
 colnames(expCounts) <- paste(suffix, colnames(counts), sep='.')
 colnames(expErcc) <- paste(suffix, colnames(counts.ercc), sep='.')
 
@@ -44,7 +35,20 @@ s.J1 <- syntheticData[ ,grepl("s.J1", colnames(syntheticData))][ ,1:85]
 m.A1B1C1D1 <- syntheticData[ ,'m.A1B1C1D1']
 m.G1H1I1J1 <- syntheticData[ ,'m.G1H1I1J1']
 
-counts <- cbind(s.A1, s.B1, s.C1, s.D1, s.E1, s.F1, s.G1, s.H1, s.I1, s.J1, m.A1B1C1D1, m.G1H1I1J1)
+counts <- cbind(
+    s.A1,
+    s.B1,
+    s.C1,
+    s.D1,
+    s.E1,
+    s.F1,
+    s.G1,
+    s.H1,
+    s.I1,
+    s.J1,
+    m.A1B1C1D1,
+    m.G1H1I1J1
+)
 
 #minimise genes
 .ntopMax <- function(data, n) {
@@ -66,21 +70,25 @@ rownames(testCounts) <- sort(
     )[1:nrow(testCounts)]
 )
 
-testErcc <- matrix(
-    c(c(
-        colSums(testCounts)[1:850]/100,
-        colSums(testCounts)[851:852]/400
-    ),
-    c(
-        colSums(testCounts)[1:850]/10,
-        colSums(testCounts)[851:852]/40
-    )),
-    nrow=2
-)
+#make testErcc
+s <- grepl("^s", colnames(expCounts))
+s2 <- grepl("^s", colnames(testCounts))
+set.seed(999)
+singletsE <- expErcc[,s]
+singletsE <- singletsE[,sample(
+    1:ncol(expErcc[,s]),
+    size=length(s2[s2 == TRUE]),
+    replace=TRUE
+)]
 
-#cObj <- spCounts(testCounts, testErcc, "m.")
-#uObj <- spUnsupervised(cObj, max=250, max_iter=1000)
-#sObj <- spSwarm(uObj, swarmsize = 150, cores=2, cutoff=0.14)
+multipletsE <-expErcc[,!s]
+multipletsE <- multipletsE[,
+    sample(1:ncol(expErcc[,!s]),
+    size=length(s2[s2 == FALSE]),
+    replace=TRUE
+)]/2
+
+testErcc <- matrix(c(singletsE, multipletsE), ncol=ncol(testCounts))
 
 save(testErcc, file="data/testErcc.rda", compress="bzip2")
 save(testCounts, file="data/testCounts.rda", compress="bzip2")
