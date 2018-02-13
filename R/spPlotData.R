973f1cffc6877ccf99dbbd5a574d56a3d739fc6e
jasonserviss <jason.serviss@ki.se>
2018-02-13 19:46:03 +0100

    moving plotData functions to spPlotData.R file

---
+++ R/spPlotData.R
@@ -0,0 +1,203 @@
+#' plotCountsData
+#'
+#' Assembles all data for plotCounts plots.
+#'
+#' @name plotCountsData
+#' @rdname plotCountsData
+#' @aliases plotCountsData
+#' @param x An spUnsupervised object.
+#' @param y An spCounts object containing singlets.
+#' @param ... additional arguments to pass on.
+#' @return A tibble with columns:
+#' @author Jason T. Serviss
+#' @keywords plotCountsData
+#' @examples
+#' #
+#'
+NULL
+
+#' @rdname plotCountsData
+#' @export
+
+setGeneric("plotCountsData", function(
+  spCountsSng,
+  ...
+){
+  standardGeneric("plotCountsData")
+})
+
+#' @rdname plotCountsData
+#' @export
+#' @import ggplot2
+#' @importFrom dplyr "%>%" rename mutate select full_join
+#' @importFrom readr parse_factor
+
+setMethod("plotCountsData", "spCounts", function(
+  spCountsSng,
+  spCountsMul,
+  markers = NULL,
+  ...
+){
+  s <- getData(spCountsSng, "counts.log")
+  m <- getData(spCountsMul, "counts.log")
+  
+  .processMarkers(cbind(s, m), markers) %>%
+  full_join(
+    estimateCells(spCountsSng, spCountsMul),
+    by = c("Sample" = "sampleName")
+  ) %>%
+  mutate(`Sample type` = parse_factor(
+    sampleType,
+    levels = c("Singlet", "Multiplet")
+  )) %>%
+  rename(`Cell number` = cellNumberMedian) %>%
+  select(Sample, `Sample type`, frac.ercc, `Cell number`, 2:3)
+})
+
+#get and process data for markers plot
+.processMarkers <- function(counts.log, markers) {
+  
+  if(is.null(markers)) {
+    return(tibble(Sample = colnames(counts.log)))
+  }
+  
+  #check that specified markers exist in data
+  if(!all(markers %in% rownames(counts.log))) {
+    notFound <- markers[!markers %in% rownames(counts.log)]
+    notFound <- paste(notFound, collapse = ", ")
+    message <- "These markers were not found in the dataset:"
+    stop(paste(message, notFound))
+  }
+  
+  #normalize the marker expression
+  #!!!!! THIS WILL ERROR IF length(markers) == 1 !!!!!!!!!!!!!!
+  markExpress <- t(counts.log[rownames(counts.log) %in% markers, ])
+  markExpressNorm <- apply(markExpress, 2, function(x) {
+    (x - min(x)) / (max(x) - min(x))
+  })
+  
+  #tidy markers
+  markExpressNorm %>%
+  matrix_to_tibble(.) %>%
+  rename(Sample = rowname)
+}
+
+#' plotUnsupervisedData
+#'
+#' Assembles all data for plotUnsupervised plots.
+#'
+#' @name plotUnsupervisedData
+#' @rdname plotUnsupervisedData
+#' @aliases plotUnsupervisedData
+#' @param x An spUnsupervised object.
+#' @param y An spCounts object containing singlets.
+#' @param ... additional arguments to pass on.
+#' @return A tibble with columns:
+#' @author Jason T. Serviss
+#' @keywords plotUnsupervisedData
+#' @examples
+#' #
+#'
+NULL
+
+#' @rdname plotUnsupervisedData
+#' @export
+
+setGeneric("plotUnsupervisedData", function(
+    x,
+    ...
+){
+    standardGeneric("plotUnsupervisedData")
+})
+
+#' @rdname plotUnsupervisedData
+#' @export
+#' @import ggplot2
+#' @importFrom dplyr "%>%" rename group_by ungroup mutate arrange summarize select full_join
+#' @importFrom tibble tibble rownames_to_column as_tibble add_column
+#' @importFrom tidyr gather unnest spread
+#' @importFrom purrr pmap
+#' @importFrom grDevices col2rgb
+#' @importFrom readr parse_factor
+
+setMethod("plotUnsupervisedData", "spUnsupervised", function(
+  x,
+  y,
+  markers = NULL,
+  pal = NULL,
+  ...
+){
+  
+  #check that if markers is ! NULL that pal is also ! NULL
+  
+  tidyUnsupervised(x) %>%
+  #add colors
+  full_join(
+    .col.from.targets(pal, getData(y, "counts"), markers),
+    by = "Sample"
+  ) %>%
+  #add marker data
+  full_join(
+    .processMarkers(getData(y, "counts.log"), markers),
+    by = "Sample"
+  )
+})
+
+#pal: a colour palette
+#targets: gene names
+#values: gene counts
+.col.from.targets <- function(
+  pal,
+  values,
+  markers,
+  ...
+){
+  if(is.null(markers) | is.null(pal)) {
+    samples <- colnames(values)
+    return(tibble(Sample = samples))
+  }
+  
+  markers <- sort(markers)
+  pal <- pal[1:length(markers)]
+  
+  values[rownames(values) %in% markers, ] %>%
+  as.data.frame() %>%
+  rownames_to_column(var = "geneName") %>%
+  as_tibble() %>%
+  gather(Sample, count, -geneName) %>%
+  #normalize
+  group_by(geneName) %>%
+  mutate(normalized = (count - min(count)) / (max(count) - min(count))) %>%
+  ungroup() %>%
+  #calculate fraction
+  group_by(Sample) %>%
+  mutate(fraction = normalized / sum(normalized)) %>%
+  mutate(fraction = if_else(is.nan(fraction), 1 / n(), fraction)) %>%
+  #setup initial hex colors
+  group_by(Sample) %>%
+  arrange(geneName) %>%
+  mutate(colint = pal) %>%
+  ungroup() %>%
+  #convert to rgb and calculate new colors
+  mutate(rgb = pmap(list(colint, normalized, fraction), function(x, y, z) {
+    (255 - ((255 - col2rgb(x)) * y)) * z
+  })) %>%
+  unnest() %>%
+  add_column(col = rep(c("r", "g", "b"), nrow(.) / 3)) %>%
+  group_by(Sample, col) %>%
+  summarize(sumRGB = sum(rgb) / 256) %>%
+  ungroup() %>%
+  spread(col, sumRGB) %>%
+  #convert back to hex
+  mutate(Colour = pmap_chr(list(r, g, b), function(x, y, z) {
+    rgb(red = x, green = y, blue = z)
+  })) %>%
+  select(-(b:r)) %>%
+  #fix factor levels so ggplot legend will cooperate
+  #https://community.rstudio.com/t/drop-false-with-scale-fill-identity/5163/2
+  mutate(Colour = parse_factor(
+    Colour,
+    levels = unique(c(Colour, pal[!pal %in% Colour]))
+  ))
+}
+
